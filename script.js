236fd82cf8dc3fcfdd143e60b6ae6d02ee045938
Aviad Zer <aviad1486@gmail.com>
2025-02-24 15:08:48 +0200

    grid-layout-style

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Smooth scrolling for navigation links
     const navLinks = document.querySelectorAll("nav ul li a");
 
     navLinks.forEach(link => {
         link.addEventListener("click", event => {
-            event.preventDefault(); // Prevent default jump
+            event.preventDefault();
             const targetId = link.getAttribute("href").slice(1);
             const targetSection = document.getElementById(targetId);
 
@@ -17,22 +18,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change slide every 5 seconds
-}
-
-function changeSlide(n) {
-    slideIndex += n - 1;
-    showSlides();
-}
 
