ae50300d020bb144b3a630e36b00fc164a680dc9
Aviad Zer <aviad1486@gmail.com>
2025-02-24 13:27:20 +0200

    adding-a-gallery

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
+
+function changeSlide(n) {
+    slideIndex += n - 1;
+    showSlides();
+}
+
